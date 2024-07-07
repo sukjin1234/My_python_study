97da2eb64facb6d47afb782c61df0f85fee9cbf8
sukjin1234 <tjrwls5661@naver.com>
2024-07-07 14:22:50 +0900

    udemy day 1 수업 정리

---
+++ udemy_1day.py
@@ -0,0 +1,8 @@
+print(len(input())) #len() : () 안에 있는 값의 길이를 알려줌
+
+two_digit_number = input() #32를 입력
+first_digit_number = two_digit_number[0]
+second_digit_number = two_digit_number[1]
+print(first_digit_number) #입력 받은 string 값을 배열을 통해
+print(second_digit_number) #나누어 저장할 수 있음
+#input() 으로 저장한 변수는 type이 string임
